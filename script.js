16f13d837bdc704bb4f41ba800dd165ee49fe849
Pedro <pedroornelaspego@gmail.com>
2022-09-07 18:50:20 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,72 @@
+// Data retrieved https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature
+Highcharts.chart('container', {
+    chart: {
+        type: 'spline'
+    },
+    title: {
+        text: 'Monthly Average Temperature'
+    },
+   
+    xAxis: {
+        categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun',
+            'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        accessibility: {
+            description: 'Months of the year'
+        }
+    },
+    yAxis: {
+        title: {
+            text: 'Temperature'
+        },
+        labels: {
+            formatter: function () {
+                return this.value + '°';
+            }
+        }
+    },
+    tooltip: {
+        crosshairs: true,
+        shared: true
+    },
+    plotOptions: {
+    series: {
+            cursor: 'pointer',
+            },
+        spline: {
+            marker: {
+                radius: 4,
+                lineColor: '#666666',
+                lineWidth: 1
+            }
+        }
+    },
+    series: [{
+        name: 'Tokyo',
+        marker: {
+            symbol: 'square'
+        },
+        data: [5.2, 5.7, 8.7, 13.9, 18.2, 21.4, 25.0, {
+            y: 26.4,
+            marker: {
+                symbol: 'url(https://www.highcharts.com/samples/graphics/sun.png)'
+            },
+            accessibility: {
+                description: 'Sunny symbol, this is the warmest point in the chart.'
+            },
+            credits:{style:{cursor:'pointer'}}, 
+            events: {
+                click: function (event) {
+                    alert(
+                        this.name + ' clicked\n' +
+                        'Alt: ' + event.altKey + '\n' +
+                        'Control: ' + event.ctrlKey + '\n' +
+                        'Meta: ' + event.metaKey + '\n' +
+                        'Shift: ' + event.shiftKey
+                    );
+                },
+            },    
+            
+        }, 22.8, 17.5, 12.1, 7.6]
+
+    }]
+});
